5e45e77f238e93a151d91d05ff5095acb108cdd9
Noah <noahhughesph@gmail.com>
2023-07-02 18:00:37 -0400

    adds fourth generation pokemon

--- src/APICalls.js
+++ src/APICalls.js
@@ -1,6 +1,6 @@
 const fetchInitialPreviewPokemonData = async () => {
   const response = await fetch(
-    'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=386'
+    'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=493'
   );
   const data = await response.json();
   return data.results;
--- src/DOMManipulation.js
+++ src/DOMManipulation.js
@@ -1,14 +1,3 @@
-/*
-        <div class="pokemon-search">
-          <img
-            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/emerald/149.png"
-          />
-          <div class="list-text">
-            <span style="width: 100%">Dragonite</span>
-            <span>#149</span>
-          </div>
-        </div>
-*/
 const typeFilterDiv = document.querySelector('#search-filter');
 
 const renderInitialSearchPokemonCard = async () => {
@@ -21,7 +10,7 @@ const renderTypeSearchPokemonCard = async (type) => {
   const pokemonData = await fetchPokemonTypeData(type);
   const filteredPokemonData = pokemonData.filter((element) => {
     const elementPokemon = element.pokemon;
-    if (pokemonIDHelper(elementPokemon.url) <= 386) {
+    if (pokemonIDHelper(elementPokemon.url) <= 493) {
       return element;
     }
   });
